c5b1c654017875801a1915c1804ea46bfa1b2ed1
Nicholas Gentile <8gentile@gmail.com>
2019-05-06 11:44:00 -0400

    build: new build

--- lib/pack/react-d3-circlepack.d.ts
+++ lib/pack/react-d3-circlepack.d.ts
@@ -1,32 +1,36 @@
 import { HierarchyCircularNode } from 'd3-hierarchy';
 import * as React from 'react';
 export interface ReactD3CirclePackDefaultProps {
-    height: string;
-    width: string;
+    height?: number;
+    width?: number;
 }
-export interface ICircleNode<Datum> {
-    data?: Datum;
-    color?: string;
-    children?: ICircleNode<Datum>;
-    key: string;
-    value?: number;
+export interface ReactD3CirclePackAccessorProps<Datum> {
+    keyAccessor: (d: Datum) => string;
+    valueAccessor: (d: Datum) => number;
+    children: (d: Datum) => Datum[];
+    colorConfigAccessor: (d: Datum) => string;
 }
-export interface ReactD3CirclePackProps<Datum> {
-    height: string;
-    width: string;
+export interface ReactD3CirclePackProps<Datum> extends ReactD3CirclePackDefaultProps {
     data: Datum;
 }
 export interface ReactD3CirclePackState<Datum> {
-    nodes: HierarchyCircularNode<{}>[];
+    data: ICirclePackNode<Datum>;
+    height: number;
+    nodes: HierarchyCircularNode<ICirclePackNode<Datum>>[];
+    width: number;
+}
+export interface ICirclePackNode<Datum> {
+    data?: Datum;
+    color?: string;
+    children?: ICirclePackNode<Datum>;
+    key: string;
+    value: number;
 }
 export default class ReactD3CirclePack<Datum> extends React.Component<ReactD3CirclePackProps<Datum>, ReactD3CirclePackState<Datum>> {
     static defaultProps: ReactD3CirclePackDefaultProps;
-    constructor(props: any);
-    assignInternalProperties: ({ data, height, width }: {
-        data: any;
-        height: any;
-        width: any;
-    }) => HierarchyCircularNode<{}>[];
-    renderCircleNodes: (nodes: HierarchyCircularNode<{}>[]) => JSX.Element[];
+    constructor(props: any, defaultProps: any);
+    private assignInternalProperties;
+    private getHierarchyCirclularNodes;
+    private renderCircleNodes;
     render(): JSX.Element;
 }
