6a0b5e8c49277104a7f5bc3e869446ddffa8b8ee
Nicholas Gentile <8gentile@gmail.com>
2019-05-11 12:28:50 -0400

    build: clean build

--- lib/pack/react-d3-circlepack.d.ts
+++ lib/pack/react-d3-circlepack.d.ts
@@ -1,17 +1,15 @@
 import { HierarchyCircularNode } from 'd3-hierarchy';
 import * as React from 'react';
-export interface ReactD3CirclePackDefaultProps {
+export interface ReactD3CirclePackAccessorProps<Datum> {
+    keyAccessor?: (d: Datum) => string;
+    valueAccessor?: (d: Datum) => number;
+    childAccessor?: (d: Datum) => Datum[];
+    colorConfigAccessor?: (d: Datum) => string;
+}
+export interface ReactD3CirclePackProps<Datum> extends ReactD3CirclePackAccessorProps<Datum> {
+    data: Datum;
     height?: number;
     width?: number;
-}
-export interface ReactD3CirclePackAccessorProps<Datum> {
-    keyAccessor: (d: Datum) => string;
-    valueAccessor: (d: Datum) => number;
-    children: (d: Datum) => Datum[];
-    colorConfigAccessor: (d: Datum) => string;
-}
-export interface ReactD3CirclePackProps<Datum> extends ReactD3CirclePackDefaultProps {
-    data: Datum;
 }
 export interface ReactD3CirclePackState<Datum> {
     data: ICirclePackNode<Datum>;
@@ -22,12 +20,21 @@ export interface ReactD3CirclePackState<Datum> {
 export interface ICirclePackNode<Datum> {
     data?: Datum;
     color?: string;
-    children?: ICirclePackNode<Datum>;
-    key: string;
-    value: number;
+    _children?: ICirclePackNode<Datum>;
+    _key: string;
+    _value: number;
 }
 export default class ReactD3CirclePack<Datum> extends React.Component<ReactD3CirclePackProps<Datum>, ReactD3CirclePackState<Datum>> {
-    static defaultProps: ReactD3CirclePackDefaultProps;
+    static defaultProps: {
+        height: number;
+        width: number;
+        keyAccessor: (d: any) => any;
+        valueAccessor: (d: any) => any;
+        childAccessor: (d: any) => any;
+    };
+    static value: symbol;
+    static children: symbol;
+    static key: symbol;
     constructor(props: any, defaultProps: any);
     private assignInternalProperties;
     private getHierarchyCirclularNodes;
